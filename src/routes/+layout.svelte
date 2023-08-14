ca02ec00f0144a17a8f1a5156a7494d458aae89a
paoloricciuti <ricciutipaolo@gmail.com>
2023-08-14 15:16:49 +0200

    some styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,29 @@
 </script>
 
 <main class="relative grid h-full grid-cols-main grid-rows-main overflow-y-auto">
-	<header class="sticky top-0 col-start-1 col-end-[-1] bg-current p-2 font-mono sm:col-end-[-2]">
+	<header class="sticky top-0 col-start-1 col-end-[-1] bg-current p-2 font-mono lg:col-end-[-2]">
 		<h1 class="text-2xl">ricciuti.me</h1>
 		<p class="text-xs">tech blog and personal website of a mad scientist</p>
+		<ul class="mt-4 flex flex-wrap gap-2">
+			<li><a href="/">home</a></li>
+			<li><a href="/about">about</a></li>
+			<li><a href="/speaking">speaking</a></li>
+			<li><a href="/contacts">contacts</a></li>
+		</ul>
 	</header>
-	<picture class="col-span-full max-w-xs p-4 sm:col-span-1">
+	<picture class="col-span-full max-w-xs p-4 lg:col-span-1">
 		<Logo width="100%" />
 	</picture>
-	<aside class="relative hidden sm:block">aside something much longer</aside>
 
-	<section class="p-4">
+	<section class="prose dark:prose-invert md:prose-md p-4 font-mono">
 		<slot />
 	</section>
-	<section class="hidden sm:block">suggestions</section>
+	<section class="hidden lg:block">suggestions</section>
 </main>
+
+<style>
+	li:not(:last-child)::after {
+		content: '|';
+		margin: theme(spacing.2);
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './mdsvex.config.js'],
 	theme: {
 		extend: {
 			backgroundColor: {
 				current: 'var(--bg)'
 			},
 			gridTemplateColumns: {
-				main: 'auto 1fr auto'
+				main: '1fr auto'
 			},
 			gridTemplateRows: {
 				main: 'auto auto 1fr'
 			}
 		}
 	},
-	plugins: []
+	plugins: [require('@tailwindcss/typography')]
 };
