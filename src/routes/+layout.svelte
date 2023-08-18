d609e77fefff8d1d90227e5cd82f342074a363ff
paoloricciuti <ricciutipaolo@gmail.com>
2023-08-18 12:42:41 +0200

    update layout with matia recommendations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,44 @@
 <script lang="ts">
 	import '../app.css';
 	import Logo from '$lib/components/Logo.svelte';
+	import { page } from '$app/stores';
+
+	const pages = {
+		home: '/',
+		about: '/about',
+		speaking: '/speaking',
+		contacts: '/contacts'
+	};
 </script>
 
-<main class="relative grid h-full grid-cols-main grid-rows-main overflow-y-auto">
-	<header class="sticky top-0 col-start-1 col-end-[-1] bg-current p-2 font-mono lg:col-end-[-2]">
-		<h1 class="text-2xl">ricciuti.me</h1>
-		<p class="text-xs">tech blog and personal website of a mad scientist</p>
-		<ul class="mt-4 flex flex-wrap gap-2">
-			<li><a href="/">home</a></li>
-			<li><a href="/about">about</a></li>
-			<li><a href="/speaking">speaking</a></li>
-			<li><a href="/contacts">contacts</a></li>
-		</ul>
-	</header>
-	<picture class="col-span-full max-w-xs p-4 lg:col-span-1">
+<header
+	class="sticky top-0 z-50 m-auto grid w-full max-w-7xl place-items-center gap-2 bg-current p-4 font-mono"
+>
+	<h1 class="text-4xl">ricciuti.me</h1>
+	<p class="text-xs opacity-50">tech blog and personal website of a mad scientist</p>
+	<ul class="mt-4 flex flex-wrap justify-center gap-2">
+		{#each Object.entries(pages) as [title, href]}
+			{@const current_page = $page.url.pathname === href}
+			<li>
+				<a {href} class:text-brand-600={current_page} class:dark:text-brand-500={current_page}
+					>{title}</a
+				>
+			</li>
+		{/each}
+	</ul>
+</header>
+<main class="relative m-auto grid max-w-7xl place-items-center">
+	<picture class="max-w-xs p-4 lg:col-span-1">
 		<Logo width="100%" />
 	</picture>
 
 	<section class="md:prose-md prose min-w-full p-4 font-mono dark:prose-invert">
 		<slot />
 	</section>
-	<section class="hidden lg:block">suggestions</section>
 </main>
 
 <style>
 	li:not(:last-child)::after {
 		content: '|';
-		margin: theme(spacing.2);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,17 @@
 	hello, fellow internet surfer, here lies the personal website and tech blog of paolo ricciuti,
 	sometimes known as pablopang.
 </p>
+<p>i speak about a lot of things in this blog but mainly about svelte 🧡</p>
+<p>here some of my latest blog posts for your reading pleasure</p>
 {#each data.articles as article}
 	<article class="my-12 grid gap-2">
-		<a class="text-xl font-bold no-underline" href="/blog/{article.slug}">{article.title}</a>
+		<a class="text-5xl font-bold no-underline" href="/blog/{article.slug}">{article.title}</a>
 		<section class="border-t-[1px] border-slate-500 p-4 pr-0">
 			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 			{@html article.preview}
 		</section>
-		<a
-			class="justify-self-start bg-slate-200 p-2 px-4 no-underline dark:bg-slate-950"
-			href="/blog/{article.slug}">full article</a
+		<a class="bg-brand-500 justify-self-start p-2 px-4 no-underline" href="/blog/{article.slug}"
+			>full article</a
 		>
 	</article>
 {/each}
