07d2ff71301398ae147149ff5252d1f70b3b3065
paoloricciuti <ricciutipaolo@gmail.com>
2023-09-04 15:31:56 +0200

    Add view transition api to theme switcher

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -4,10 +4,18 @@
 	import { fade, crossfade } from 'svelte/transition';
 
 	const [send, receive] = crossfade({
-		duration: 250
+		duration: 250,
 	});
 
 	let theme: 'light' | 'dark' | undefined;
+
+	function swap() {
+		document.documentElement.classList.remove(theme ?? '');
+		theme = theme === 'dark' ? 'light' : 'dark';
+		window.localStorage.setItem('ricciuti_me_theme', theme);
+		document.documentElement.classList.add(theme);
+	}
+
 	onMount(() => {
 		const current_theme = window.localStorage.getItem('ricciuti_me_theme');
 		if (!current_theme) {
@@ -28,24 +36,60 @@
 {#if theme}
 	<button
 		in:fade={{
-			duration: 100
+			duration: 100,
 		}}
-		on:click={() => {
-			document.documentElement.classList.remove(theme ?? '');
-			theme = theme === 'dark' ? 'light' : 'dark';
-			window.localStorage.setItem('ricciuti_me_theme', theme);
-			document.documentElement.classList.add(theme);
+		on:click={async (e) => {
+			const prefers_reduced_motion = window.matchMedia('(prefers-reduced-motion: reduce)');
+			if (!document.startViewTransition || prefers_reduced_motion.matches) {
+				swap();
+				return;
+			}
+
+			const { x, y, width, height } = e.currentTarget.getBoundingClientRect();
+			const start_x = x + width / 2;
+			const start_y = y + height / 2;
+
+			const end_radius = Math.hypot(
+				Math.max(start_x, innerWidth - start_x),
+				Math.max(start_y, innerHeight - start_y),
+			);
+
+			document.documentElement.classList.add('swapping_theme');
+			const transition = document.startViewTransition(() => {
+				swap();
+			});
+
+			// Wait for the pseudo-elements to be created:
+			transition.ready.then(async () => {
+				// Animate the root's new view
+				document.documentElement.animate(
+					{
+						clipPath: [
+							`circle(0 at ${start_x}px ${start_y}px)`,
+							`circle(${end_radius}px at ${start_x}px ${start_y}px)`,
+						],
+					},
+					{
+						duration: 500,
+						easing: 'ease-in',
+						// Specify which pseudo-element to animate
+						pseudoElement: '::view-transition-new(root)',
+					},
+				);
+			});
+			await transition.finished;
+			document.documentElement.classList.remove('swapping_theme');
 		}}
-		class="2xs:fixed 2xs:right-4 2xs:top-4 relative z-50 h-8 w-8 justify-self-end"
+		class="relative z-50 h-8 w-8 justify-self-end 2xs:fixed 2xs:right-4 2xs:top-4"
 	>
 		{#if theme === 'light'}
 			<span
 				class="absolute inset-0 grid place-items-center"
 				in:send={{
-					key: 'theme'
+					key: 'theme',
 				}}
 				out:receive={{
-					key: 'theme'
+					key: 'theme',
 				}}
 			>
 				<Moon />
@@ -54,10 +98,10 @@
 			<span
 				class="absolute inset-0 grid place-items-center"
 				in:send={{
-					key: 'theme'
+					key: 'theme',
 				}}
 				out:receive={{
-					key: 'theme'
+					key: 'theme',
 				}}
 			>
 				<Sun />
@@ -65,3 +109,13 @@
 		{/if}
 	</button>
 {/if}
+
+<style>
+	:global(.swapping_theme::view-transition-old(root)) {
+		animation: none;
+		mix-blend-mode: normal;
+	}
+	:global(.swapping_theme::view-transition-new(root)) {
+		mix-blend-mode: normal;
+	}
+</style>
