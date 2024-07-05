c382b32734b0ab56d4756a3dec486a39149f6798
paoloricciuti <ricciutipaolo@gmail.com>
2024-07-06 00:31:39 +0200

    chore: remove unused vite plugin and setup renovate

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import dynamic_import from 'vite-plugin-dynamic-import';
 
 export default defineConfig({
-	plugins: [dynamic_import(), sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
